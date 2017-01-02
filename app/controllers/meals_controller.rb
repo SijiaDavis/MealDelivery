0b4af26fe914f117b122db2531e06c9a16046cca
SijiaDavis <arwen@ksu.edu>
2017-01-02 13:46:21 -0500

    Add meal index page

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,11 +1,25 @@
 class MealsController < ApplicationController
+  
+  def index
+    @meals = Meal.all
+  end
+  
   def new
     @meal = Meal.new
+  end
+  
+  def create 
+    @meal = Meal.create(meal_params)
+    if @meal.valid?
+      redirect_to meals_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   
   private
   
   def meal_params
-    
+    params.require(:meal).permit(:name, :ingredient, :category, :cost, :high_protein, :low_carb, :vegan)
   end
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,15 @@
 class Meal < ApplicationRecord
+  
+  validates :name, presence: true
+  validates :ingredient, presence: true
+  validates :category, presence: true
+  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
+  
+  CATEGORIES = {
+    'breakfast' => 'Breakfast',
+    'main' => 'main',
+    'dessert' => 'dessert',
+    'side' => 'side'
+  }
+  
 end
